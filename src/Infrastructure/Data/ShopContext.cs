f5ab91353ddc284e571ef3d492c6f1729338e2c7
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2023-03-28 12:23:47 +0300

    Db Context Precision

---
+++ src/Infrastructure/Data/ShopContext.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+            
+        }
+
+       public DbSet<Brand>
+    }
+}
