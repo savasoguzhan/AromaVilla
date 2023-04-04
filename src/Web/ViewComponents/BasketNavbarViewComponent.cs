2cf449d1f3e7f9cfc0513ee5872117d6247db486
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2023-04-04 12:10:21 +0300

    basket

--- src/Web/ViewComponents/BasketNavbarViewComponent.cs
+++ src/Web/ViewComponents/BasketNavbarViewComponent.cs
@@ -1,6 +1,20 @@
-﻿namespace Web.ViewComponents
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Interfaces;
+
+namespace Web.ViewComponents
 {
-    public class BasketNavbarViewComponent
+    public class BasketNavbarViewComponent : ViewComponent
     {
+        private readonly IBasketViewModelService _basketViewModelService;
+
+        public BasketNavbarViewComponent(IBasketViewModelService basketViewModelService)
+        {
+            _basketViewModelService = basketViewModelService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _basketViewModelService.GetBasketViewModel());
+        }
     }
 }
